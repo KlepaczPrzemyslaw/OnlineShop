eec688772ccd1f7d19445a2a057243d687dd3282
Przemyslaw Klepacz <przemyslaw.klepacz.mobile@gmail.com>
2018-09-24 00:32:50 +0200

    OnlineShop V1.07

--- OnlineShop/Context/DbInitializer.cs
+++ OnlineShop/Context/DbInitializer.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using OnlineShop.Models;
 
 namespace OnlineShop.Context
 {
@@ -71,6 +73,67 @@ namespace OnlineShop.Context
 				if (adminSuccess.Succeeded)
 				{
 					await userManager.AddToRoleAsync(await userManager.FindByNameAsync(userLogin), "User");
+				}
+
+				// PRODUCTS
+				if (context.ProductModel.FirstOrDefault(x => x.Name == "Example") == null)
+				{
+					context.ProductModel.Add(new ProductModel()
+					{
+						Category = "First Example",
+						Description = "Good product!",
+						Name = "Example",
+						PhotoID = Guid.Parse("12b8531d-50de-4577-b581-7fe7762c1bc3"),
+						Price = 10.99M,
+						Quantity = 10,
+						Updated = DateTime.UtcNow
+					});
+					context.SaveChanges();
+				}
+
+				if (context.ProductModel.FirstOrDefault(x => x.Name == "Example 2") == null)
+				{
+					context.ProductModel.Add(new ProductModel()
+					{
+						Category = "Second Example",
+						Description = "Good product!",
+						Name = "Example 2",
+						PhotoID = Guid.Parse("12b8531d-50de-4577-b581-7fe7762c1bc3"),
+						Price = 39.99M,
+						Quantity = 50,
+						Updated = DateTime.UtcNow
+					});
+					context.SaveChanges();
+				}
+
+				if (context.ProductModel.FirstOrDefault(x => x.Name == "Example 3") == null)
+				{
+					context.ProductModel.Add(new ProductModel()
+					{
+						Category = "Second Example",
+						Description = "Good product!",
+						Name = "Example 3",
+						PhotoID = Guid.Parse("12b8531d-50de-4577-b581-7fe7762c1bc3"),
+						Price = 50.50M,
+						Quantity = 3,
+						Updated = DateTime.UtcNow
+					});
+					context.SaveChanges();
+				}
+
+				if (context.ProductModel.FirstOrDefault(x => x.Name == "Example 4") == null)
+				{
+					context.ProductModel.Add(new ProductModel()
+					{
+						Category = "Third Example",
+						Description = "Good product!",
+						Name = "Example 4",
+						PhotoID = Guid.Parse("12b8531d-50de-4577-b581-7fe7762c1bc3"),
+						Price = 150.00M,
+						Quantity = 30,
+						Updated = DateTime.UtcNow
+					});
+					context.SaveChanges();
 				}
 			}
 		}
